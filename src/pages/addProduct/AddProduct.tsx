de188055a5fc6bc76f54adbb8124f44f2659ee2d
Mashhur <mashhuryoldoshev2003@gmail.com>
2023-08-03 13:09:12 +0800

    addProduct

--- src/pages/addProduct/AddProduct.tsx
+++ src/pages/addProduct/AddProduct.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import "./addProduct.css";
+import Notiflix from "notiflix";
 
 interface ProductData {
   id: number;
@@ -27,6 +28,7 @@ const newProductProps = [
 
 function AddProduct() {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [diable, setDisable] = useState(true);
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -45,6 +47,9 @@ function AddProduct() {
       ...prevProduct,
       img: [...prevProduct.img, ...imageUrls],
     }));
+    setDisable(false);
+    Notiflix.Notify.success("Rasmlar yuklandi");
+    setSelectedFiles([]);
   };
 
   const latestSelectedFile =
@@ -68,106 +73,108 @@ function AddProduct() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setnewProduct(newProductProps[0]);
-    setSelectedFiles([]);
     console.log(newProduct);
   };
 
   return (
     <section id="addProduct">
       <h1>Yangi Tavar Qo'shish</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="container">
-          <div className="addImgContainer">
-            <h2>Suratlarni tanlang</h2>
-            <input type="file" multiple onChange={handleFileChange} />
-            <button
-              onClick={handleUpload}
-              style={{ display: selectedFiles.length === 0 ? "none" : "block" }}
-            >
-              Rasmlarn yuklash
-            </button>
-            {latestSelectedFile && (
-              <div className="single-pro-img">
-                <img
-                  src={URL.createObjectURL(latestSelectedFile)}
-                  width="100%"
-                  id="mainImg"
-                  alt="Main"
-                />
-                {selectedFiles.length > 1 && (
-                  <div className="small-img-group">
-                    {selectedFiles
-                      .slice(0, selectedFiles.length - 1)
-                      .map((file, index) => (
-                        <div className="small-img-col" key={index}>
-                          <img
-                            src={URL.createObjectURL(file)}
-                            width="100%"
-                            className="small-img"
-                            alt={`Small ${index + 1}`}
-                          />
-                        </div>
-                      ))}
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
 
-          <div className="addProductText">
-            <h2>Mantlarni krting</h2>
-
-            <input
-              type="text"
-              onChange={handleChange}
-              name="cname"
-              placeholder="Ko'mpaniya"
-              required
-            />
-            <input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              placeholder="Tavar nomi"
-              required
-            />
-            <input
-              type="number"
-              name="price"
-              onChange={handleChange}
-              placeholder="Narxi"
-              required
-            />
-            <input
-              type="text"
-              name="sizes"
-              onChange={handleChange}
-              placeholder="Razmerlar vergul ( ,) blan ajrating "
-              required
-            />
-
-            <select name="category" onChange={handleChange} required>
-              <option value="">Categoriyalar</option>
-              <option value="Oyollar Koylaklar">Oyollar Koylaklar</option>
-              <option value="Erkaklar Koylaklar">Erkaklar Koylaklar</option>
-              <option value="Krasofka">Krasofka</option>
-              <option value="Bujiteriya">Bujiteriya</option>
-              <option value="Sumka">Sumka</option>
-            </select>
-            <textarea
-              onChange={handleChange}
-              rows={5}
-              name="des"
-              placeholder="Tavar haqda malumot"
-              required
-            />
-          </div>
+      <div className="container">
+        <div className="addImgContainer">
+          <h2>Suratlarni tanlang</h2>
+          <input type="file" multiple onChange={handleFileChange} />
+          <button
+            onClick={handleUpload}
+            style={{ display: selectedFiles.length === 0 ? "none" : "block" }}
+          >
+            Rasmlarn yuklash
+          </button>
+          {latestSelectedFile && (
+            <div className="single-pro-img">
+              <img
+                src={URL.createObjectURL(latestSelectedFile)}
+                width="100%"
+                id="mainImg"
+                alt="Main"
+              />
+              {selectedFiles.length > 1 && (
+                <div className="small-img-group">
+                  {selectedFiles
+                    .slice(0, selectedFiles.length - 1)
+                    .map((file, index) => (
+                      <div className="small-img-col" key={index}>
+                        <img
+                          src={URL.createObjectURL(file)}
+                          width="100%"
+                          className="small-img"
+                          alt={`Small ${index + 1}`}
+                        />
+                      </div>
+                    ))}
+                </div>
+              )}
+            </div>
+          )}
         </div>
-        <div id="btn">
-          <button type="submit">Yuklash</button>
-        </div>
-      </form>
+
+        <form onSubmit={handleSubmit} className="addProductText">
+          <h2>Mantlarni krting</h2>
+
+          <input
+            type="text"
+            onChange={handleChange}
+            name="cname"
+            placeholder="Ko'mpaniya"
+            required
+          />
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            placeholder="Tavar nomi"
+            required
+          />
+          <input
+            type="number"
+            name="price"
+            onChange={handleChange}
+            placeholder="Narxi"
+            required
+          />
+          <input
+            type="text"
+            name="sizes"
+            onChange={handleChange}
+            placeholder="Razmerlar vergul ( ,) blan ajrating "
+            required
+          />
+
+          <select name="category" onChange={handleChange} required>
+            <option value="">Categoriyalar</option>
+            <option value="Oyollar Koylaklar">Oyollar Koylaklar</option>
+            <option value="Erkaklar Koylaklar">Erkaklar Koylaklar</option>
+            <option value="Krasofka">Krasofka</option>
+            <option value="Bujiteriya">Bujiteriya</option>
+            <option value="Sumka">Sumka</option>
+          </select>
+          <textarea
+            onChange={handleChange}
+            rows={5}
+            name="des"
+            placeholder="Tavar haqda malumot"
+            required
+          />
+
+          <button
+            onClick={() => Notiflix.Notify.success("Mahsulot Yuklandi")}
+            type="submit"
+            disabled={diable}
+          >
+            Yuklash
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
